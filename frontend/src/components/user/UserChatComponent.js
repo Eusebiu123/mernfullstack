e4db5f78e63be990c8ccf0757b85a59dd5715653
Eusebiu Popescu <popescusebi1234@yahoo.com>
2023-02-26 19:16:17 +0200

    add chat

--- frontend/src/components/user/UserChatComponent.js
+++ frontend/src/components/user/UserChatComponent.js
@@ -1,5 +1,16 @@
-const UserChatComponent = () =>{
-    return <p>This is a user chat component</p>;
+import "../../chats.css";
+
+const UserChatComponent = () => {
+    return (
+        <>
+            <input type="checkbox" id="check" />
+            <label className="chat-btn" htmlFor="check">
+                <i className="bi bi-chat-dots comment"></i>
+                <i className="bi bi-x-circle close"></i>
+            </label>
+
+        </>
+    )
 }
 
 export default UserChatComponent;
