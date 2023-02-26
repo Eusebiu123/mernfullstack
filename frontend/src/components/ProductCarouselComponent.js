e44763dc71caec5aaa890a088f2e369c1cdc704e
Eusebiu Popescu <popescusebi1234@yahoo.com>
2023-02-26 18:21:11 +0200

    customize product&category

--- frontend/src/components/ProductCarouselComponent.js
+++ frontend/src/components/ProductCarouselComponent.js
@@ -1,318 +1,493 @@
-import {Carousel} from 'react-bootstrap'
+import { Carousel } from 'react-bootstrap';
+
+import {LinkContainer} from 'react-router-bootstrap';
+
+
 
 const ProductCarouselComponent = () => {
+  const cursorP={
+    cursor: "pointer"
+  }
   return (
     <Carousel>
       <Carousel.Item>
         <img
+          crossorigin="anonymous"
           className="d-block w-100"
+
+          style={{heigh:"300px", objectFit: "cover"}}
+
           src="/images/carousel/sistem_franare.jpg"
           alt="First slide"
         />
         <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          <LinkContainer style={cursorP} to="/product-details">
+          <h3>Cel mai bun sistem de franare</h3>
+
+          </LinkContainer>
+    
+          <p>Descriere</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
+          crossorigin="anonymous"
           className="d-block w-100"
+
+          style={{heigh:"300px", objectFit: "cover"}}
           src="/images/carousel/filtre.jpg"
           alt="Second slide"
         />
 
         <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <LinkContainer style={cursorP} to="/product-details">
+          <h3>Cel mai bun sistem de filtre</h3>
+
+          </LinkContainer>
+          <p>Descriere</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
+          crossorigin="anonymous"
           className="d-block w-100"
+
+          style={{heigh:"300px", objectFit: "cover"}}
           src="/images/carousel/motor.jpg"
           alt="Third slide"
         />
 
         <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
+        <LinkContainer style={cursorP} to="/product-details">
+          <h3>Cele mai bune motoare</h3>
+
+          </LinkContainer>
+          <p>Descriere</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
+          crossorigin="anonymous"
           className="d-block w-100"
+
+          style={{heigh:"300px", objectFit: "cover"}}
           src="/images/carousel/bujii.jpg"
           alt="First slide"
         />
         <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        <LinkContainer style={cursorP} to="/product-details">
+          <h3>Cel mai bun sistem de bujii</h3>
+
+          </LinkContainer>
+          <p>Descriere</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
+          crossorigin="anonymous"
           className="d-block w-100"
+
+          style={{heigh:"300px", objectFit: "cover"}}
           src="/images/carousel/suspensie.jpg"
           alt="First slide"
         />
         <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        <LinkContainer style={cursorP} to="/product-details">
+          <h3>Cel mai bun sistem de suspensie</h3>
+
+          </LinkContainer>
+          <p>Descriere</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
-          className="d-block w-100"
+         crossorigin="anonymous"
+         className="d-block w-100"
+
+         style={{heigh:"300px", objectFit: "cover"}}
           src="/images/carousel/amortizoare.jpg"
           alt="First slide"
         />
         <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        <LinkContainer style={cursorP} to="/product-details">
+          <h3>Cel mai bun sistem de amortizoare</h3>
+
+          </LinkContainer>
+          <p>Descriere</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
-          className="d-block w-100"
+         crossorigin="anonymous"
+         className="d-block w-100"
+
+         style={{heigh:"300px", objectFit: "cover"}}
           src="/images/carousel/faruri.jpg"
           alt="First slide"
         />
         <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        <LinkContainer style={cursorP} to="/product-details">
+          <h3>Cel mai bun sistem de faruri</h3>
+
+          </LinkContainer>
+          <p>Descriere</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
-          className="d-block w-100"
+         crossorigin="anonymous"
+         className="d-block w-100"
+
+         style={{heigh:"300px", objectFit: "cover"}}
           src="/images/carousel/parbriz.png"
           alt="First slide"
         />
         <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        <LinkContainer style={cursorP} to="/product-details">
+          <h3>Cel mai bun sistem de parbriz</h3>
+
+          </LinkContainer>
+          <p>Descriere</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
-          className="d-block w-100"
+         crossorigin="anonymous"
+         className="d-block w-100"
+
+         style={{heigh:"300px", objectFit: "cover"}}
           src="/images/carousel/electrice.jpg"
           alt="First slide"
         />
         <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        <LinkContainer style={cursorP} to="/product-details">
+          <h3>Cele mai bune sisteme electrice</h3>
+
+          </LinkContainer>
+          <p>Descriere</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
-          className="d-block w-100"
+         crossorigin="anonymous"
+         className="d-block w-100"
+
+         style={{heigh:"300px", objectFit: "cover"}}
           src="/images/carousel/esapament.jpg"
           alt="First slide"
         />
         <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        <LinkContainer style={cursorP} to="/product-details">
+          <h3>Cel mai bun sistem de esapement</h3>
+
+          </LinkContainer>
+          <p>Descriere</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
-          className="d-block w-100"
+         crossorigin="anonymous"
+         className="d-block w-100"
+
+         style={{heigh:"300px", objectFit: "cover"}}
           src="/images/carousel/motor.jpg"
           alt="First slide"
         />
         <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        <LinkContainer style={cursorP} to="/product-details">
+          <h3>Cele mai bune motoare</h3>
+
+          </LinkContainer>
+          <p>Descriere</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
-          className="d-block w-100"
+         crossorigin="anonymous"
+         className="d-block w-100"
+
+         style={{heigh:"300px", objectFit: "cover"}}
           src="/images/carousel/ambreiaj.png"
           alt="First slide"
         />
         <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        <LinkContainer style={cursorP} to="/product-details">
+          <h3>Cel mai bun sistem de ambreiaj</h3>
+
+          </LinkContainer>
+          <p>Descriere</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
+          crossorigin="anonymous"
           className="d-block w-100"
+
+          style={{heigh:"300px", objectFit: "cover"}}
           src="/images/carousel/curele.jpg"
           alt="First slide"
         />
         <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        <LinkContainer style={cursorP} to="/product-details">
+          <h3>Cel mai bun sistem de curele</h3>
+
+          </LinkContainer>
+          <p>Descriere</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
-          className="d-block w-100"
+        crossorigin="anonymous"
+        className="d-block w-100"
+
+        style={{heigh:"300px", objectFit: "cover"}}
           src="/images/carousel/confort.jpg"
           alt="First slide"
         />
         <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        <LinkContainer style={cursorP} to="/product-details">
+          <h3>Cel mai bun sistem de confort</h3>
+
+          </LinkContainer>
+          <p>Descriere</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
+          crossorigin="anonymous"
           className="d-block w-100"
+
+          style={{heigh:"300px", objectFit: "cover"}}
           src="/images/carousel/alimentare.jpg"
           alt="First slide"
         />
         <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        <LinkContainer style={cursorP} to="/product-details">
+          <h3>Cel mai bun sistem de alimentare</h3>
+
+          </LinkContainer>
+          <p>Descriere</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
+          crossorigin="anonymous"
           className="d-block w-100"
+
+          style={{heigh:"300px", objectFit: "cover"}}
           src="/images/carousel/planetara.jpg"
           alt="First slide"
         />
         <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        <LinkContainer style={cursorP} to="/product-details">
+          <h3>Cel mai bune planetare</h3>
+
+          </LinkContainer>
+          <p>Descriere</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
-          className="d-block w-100"
+         crossorigin="anonymous"
+         className="d-block w-100"
+
+         style={{heigh:"300px", objectFit: "cover"}}
           src="/images/carousel/directie.jpg"
           alt="First slide"
         />
         <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        <LinkContainer style={cursorP} to="/product-details">
+          <h3>Cel mai bun sistem de directie</h3>
+
+          </LinkContainer>
+          <p>Descriere</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
-          className="d-block w-100"
+         crossorigin="anonymous"
+         className="d-block w-100"
+
+         style={{heigh:"300px", objectFit: "cover"}}
           src="/images/carousel/conditionat.jpg"
           alt="First slide"
         />
         <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        <LinkContainer style={cursorP} to="/product-details">
+          <h3>Cel mai bun sistem de aer conditionat</h3>
+
+          </LinkContainer>
+          <p>Descriere</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
+          crossorigin="anonymous"
           className="d-block w-100"
+
+          style={{heigh:"300px", objectFit: "cover"}}
           src="/images/carousel/senzori.jpg"
           alt="First slide"
         />
         <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        <LinkContainer style={cursorP} to="/product-details">
+          <h3>Cel mai bun sistem de senzori</h3>
+
+          </LinkContainer>
+          <p>Descriere</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
-          className="d-block w-100"
+         crossorigin="anonymous"
+         className="d-block w-100"
+
+         style={{heigh:"300px", objectFit: "cover"}}
           src="/images/carousel/incalzire.jpg"
           alt="First slide"
         />
         <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        <LinkContainer style={cursorP} to="/product-details">
+          <h3>Cel mai bun sistem de incalzire</h3>
+
+          </LinkContainer>
+          <p>Descriere</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
-          className="d-block w-100"
+         crossorigin="anonymous"
+         className="d-block w-100"
+
+         style={{heigh:"300px", objectFit: "cover"}}
           src="/images/carousel/uleiuri.jpg"
           alt="First slide"
         />
         <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        <LinkContainer style={cursorP} to="/product-details">
+          <h3>Cele mai bune uleiuri</h3>
+
+          </LinkContainer>
+          <p>Descriere</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
-          className="d-block w-100"
+         crossorigin="anonymous"
+         className="d-block w-100"
+
+         style={{heigh:"300px", objectFit: "cover"}}
           src="/images/carousel/garnituri.png"
           alt="First slide"
         />
         <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        <LinkContainer style={cursorP} to="/product-details">
+          <h3>Cele mai bune garnituri</h3>
+
+          </LinkContainer>
+          <p>Descriere</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
+          crossorigin="anonymous"
           className="d-block w-100"
+
+          style={{heigh:"300px", objectFit: "cover"}}
           src="/images/carousel/cutie_viteze.jpg"
           alt="First slide"
         />
         <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        <LinkContainer style={cursorP} to="/product-details">
+          <h3>Cea mai buna cutie de viteze</h3>
+
+          </LinkContainer>
+          <p>Descriere</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
+          crossorigin="anonymous"
           className="d-block w-100"
+
+          style={{heigh:"300px", objectFit: "cover"}}
           src="/images/carousel/racord.jpg"
           alt="First slide"
         />
         <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        <LinkContainer style={cursorP} to="/product-details">
+          <h3>Cel mai bun sistem de racorduri</h3>
+
+          </LinkContainer>
+          <p>Descriere</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
-          className="d-block w-100"
+         crossorigin="anonymous"
+         className="d-block w-100"
+
+         style={{heigh:"300px", objectFit: "cover"}}
           src="/images/carousel/set.jpg"
           alt="First slide"
         />
         <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        <LinkContainer style={cursorP} to="/product-details">
+          <h3>Cel mai bun sistem de seturi</h3>
+
+          </LinkContainer>
+          <p>Descriere</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
-          className="d-block w-100"
+         crossorigin="anonymous"
+         className="d-block w-100"
+
+         style={{heigh:"300px", objectFit: "cover"}}
           src="/images/carousel/rulmenti.png"
           alt="First slide"
         />
         <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        <LinkContainer style={cursorP} to="/product-details">
+          <h3>Cel mai bun sistem de rulmenti</h3>
+
+          </LinkContainer>
+          <p>Descriere</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
+          crossorigin="anonymous"
           className="d-block w-100"
+
+          style={{heigh:"300px", objectFit: "cover"}}
           src="/images/carousel/arbori.jpg"
           alt="First slide"
         />
         <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        <LinkContainer style={cursorP} to="/product-details">
+          <h3>Cel mai bun sistem de arbori</h3>
+
+          </LinkContainer>
+          <p>Descriere</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
-          className="d-block w-100"
+         crossorigin="anonymous"
+         className="d-block w-100"
+
+         style={{heigh:"300px", objectFit: "cover"}}
           src="/images/carousel/tractare.jpg"
           alt="First slide"
         />
         <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        <LinkContainer style={cursorP} to="/product-details">
+          <h3>Cel mai bun sistem de tractare</h3>
+
+          </LinkContainer>
+          <p>Descriere</p>
         </Carousel.Caption>
       </Carousel.Item>
 
--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,12 +1,35 @@
 import ProductCarouselComponent from "../components/ProductCarouselComponent";
 import CategoryCardComponent from "../components/CategoryCardComponent";
+import {Row,Container} from "react-bootstrap";
 
 const HomePage = () => {
+
+    const categories= [
+        "Disc frana",
+        "Set placute",
+        "Furtun frana",
+        "Etrier frana",
+        "Set saboti frana",
+        "Cablu frana de parcare",
+        "Senzor ABS",
+        "Lichid de frana",
+        "Inel senzor",
+        "Piston",
+        "Servofrana",
+        "Conducta frana",
+        "Frana tambur",
+    ];
     return (
         <>
         <ProductCarouselComponent/>
-        <CategoryCardComponent/>
+        <Container>
+        <Row xs={1} md={2} className="g-4 mt-5">
+        {
+            categories.map( (category,idx) =>  <CategoryCardComponent key={idx} category={category} idx={idx}/>)
+        }
+        </Row>
+        </Container>
         </>
-    )
+    );
 }
 export default HomePage;
