fe6ee66cd350a327096b579e30907ce3a933f98f
Eusebiu Popescu <popescusebi1234@yahoo.com>
2023-03-02 23:27:03 +0200

    work

--- frontend/src/components/CategoryCardComponent.js
+++ frontend/src/components/CategoryCardComponent.js
@@ -1,37 +1,23 @@
 import { Card, Button } from "react-bootstrap";
-import {LinkContainer} from "react-router-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 
-const CategoryCardComponent = ({category,idx}) => {
-  const images=[
-    "/images/poza1.png",
-    "/images/poza2.png",
-    "/images/poza3.png",
-    "/images/poza1.png",
-    "/images/poza2.png",
-    "/images/poza3.png",
-    "/images/poza1.png",
-    "/images/poza2.png",
-    "/images/poza3.png",
-    "/images/poza1.png",
-    "/images/poza2.png",
-    "/images/poza3.png",
-    "/images/poza1.png",
-  ];
+const CategoryCardComponent = ({ category, idx }) => {
+  
   return (
-    <Card >
-      <Card.Img crossOrigin="anonymous" variant="top" src={images[idx]} />
+    <Card>
+      <Card.Img crossOrigin="anonymous" variant="top" src={category.image ?? null} />
       <Card.Body>
-        <Card.Title>{category}</Card.Title>
+        <Card.Title>{category.name}</Card.Title>
         <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+          {category.description}
         </Card.Text>
-        <LinkContainer to="/product-list">
-        <Button variant="primary">Go to the category</Button>
+        <LinkContainer to={`/product-list/category/${category.name}`}>
+          <Button variant="primary">Go to the Category</Button>
         </LinkContainer>
       </Card.Body>
     </Card>
   );
 };
 
-export default CategoryCardComponent
+export default CategoryCardComponent;
+
